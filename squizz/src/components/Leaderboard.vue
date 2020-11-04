a5d15132df805301dc4e1032bbdb0550364ed1a3
LeoLaurent <laurentj.leo@gmail.com>
2020-11-04 17:44:03 +0100

    modif leaderboard.vue

--- squizz/src/components/Leaderboard.vue
+++ squizz/src/components/Leaderboard.vue
@@ -1,12 +1,66 @@
 <template>
-    <v-container>
-
-    </v-container>
+  <v-simple-table height="300px">
+      <thead>
+        <tr>
+          <th class="text-left">
+            Pseudo
+          </th>
+          <th class="text-left">
+            Score
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="entry in scores"
+          :key="entry.username"
+        >
+          <td>salut</td>
+          <td>69</td>
+        </tr>
+      </tbody>
+  </v-simple-table>
 </template>
 
 <script>
+    import gql from 'graphql-tag'
     export default {
-        name: "Leaderboard.vue"
+        name: "Leaderboard.vue",
+
+        beforeMount() {
+            console.log(this.$apollo);
+            this.$apollo.queries.scores.refetch();
+        },
+
+        data () {
+          return {
+            search: '',
+            headers: [
+              {
+                text: 'Pseudo',
+                align: 'start',
+                filterable: true,
+                value: 'pseudo',
+              },
+              { text: 'Score', value: 'score' },
+            ],
+            scores: {},
+          }
+        },
+
+        apollo: {
+            scores: {
+                query() {
+                    return gql`query {
+                      scores {
+                        username
+                        score
+                      }
+                    }`
+                }
+            }
+
+        }
     }
 </script>
 
