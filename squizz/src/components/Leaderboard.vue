73ae7062e77aad15291855a640b80be1aa272f87
LeoLaurent <laurentj.leo@gmail.com>
2020-11-04 11:35:25 +0100

    add table to LeaderBoard.vue

--- squizz/src/components/Leaderboard.vue
+++ squizz/src/components/Leaderboard.vue
@@ -1,11 +1,34 @@
 <template>
-    
+    <v-simple-table dense>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">
+            Pseudo
+          </th>
+          <th class="text-left">
+            Score
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="entries for this quizz"
+          :key="item.pseudo"
+        >
+          <td>{{ entry.pseudo }}</td>
+          <td>{{ entry.score }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
 </template>
 
 <script>
-    export default {
-        name: "Leaderboard.vue"
-    }
+<script>
+export default {
+  name: "Leaderboard",
+}
 </script>
 
 <style scoped>
